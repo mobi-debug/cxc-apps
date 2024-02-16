883ce8d132763436648c883ada0c328bc94a790c
Mobi IT <71031050+mobi-debug@users.noreply.github.com>
2024-02-16 10:29:32 -0500

    Update install.sh
    added variable for checking to see if talkdesk is installed

--- install.sh
+++ install.sh
@@ -16,29 +16,24 @@ brew doctor
 brew install dockutil
 brew install --cask slack google-chrome 1password google-drive zoom chrome-remote-desktop-host
 
-# Download and install Talkdesk
-# curl -o ~/Downloads/talkdesk-1.12.0.dmg "https://td-infra-prd-us-east-1-s3-atlaselectron.s3.amazonaws.com/talkdesk-1.12.0.dmg"
-# Mount the DMG file and store the mount point path
-# MOUNT_POINT=$(hdiutil attach ~/Downloads/talkdesk-1.12.0.dmg | grep -o '/Volumes/[^ ]*')
-# Assuming Talkdesk.app is directly in the root of the mounted volume, adjust if it's in a subfolder
-# sudo cp -R "$MOUNT_POINT/Talkdesk.app" /Applications/
-# Eject the mounted DMG
-# hdiutil detach "$MOUNT_POINT"
-
-
 # Clear the dock and add specified items
 dockutil --remove all --no-restart
 dockutil --add '/Applications/Launchpad.app' --no-restart
 dockutil --add '/Applications/Google Chrome.app' --no-restart
 dockutil --add '/Applications/Slack.app' --no-restart
-# dockutil --add '/Applications/Talkdesk.app' --no-restart
+
+# Check if Talkdesk.app is installed and add it to the dock next to Slack if it is
+if [[ -d "/Applications/Talkdesk.app" ]]; then
+    dockutil --add '/Applications/Talkdesk.app' --after 'Slack' --no-restart
+fi
+
 dockutil --add '/System/Applications/Notes.app' --no-restart
 dockutil --add '/System/Applications/Reminders.app' --no-restart
 dockutil --add '/System/Applications/System Settings.app' --no-restart
 dockutil --add '~/Downloads' --view fan --display folder --sort dateadded
 
-# Clean up
-rm ~/Downloads/talkdesk-1.12.0.dmg
+# Clean up (Commented out since the Talkdesk download and install steps are also commented out)
+# rm ~/Downloads/talkdesk-1.12.0.dmg
 
 # Completion message
 echo "Installation and setup complete!"
